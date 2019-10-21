2da6498049d0664caf49096f86fbe39425a7d79c
mridul037 <30683771+mridul037@users.noreply.github.com>
2019-10-21 16:24:24 +0530

    Add files via upload

---
+++ build/precache-manifest.617198feb99796026768c8a568122c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0115deb3358ea5a6efd544ad15fc9925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a607c1284d5528229b2",
+    "url": "/static/css/main.f35bc1e3.chunk.css"
+  },
+  {
+    "revision": "08e3a62fbf7fb13088d4",
+    "url": "/static/js/2.bf1d4a21.chunk.js"
+  },
+  {
+    "revision": "7a607c1284d5528229b2",
+    "url": "/static/js/main.66e2d4ed.chunk.js"
+  },
+  {
+    "revision": "787e7d1e7468d7ff9fc3",
+    "url": "/static/js/runtime-main.cd2f7057.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
